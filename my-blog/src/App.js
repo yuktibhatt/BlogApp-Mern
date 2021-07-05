b9e63b78e5f109d0fe5d213580cdb09d7fe3a3ac
yuktibhatt <bhattyukti5@gmail.com>
2021-07-05 23:02:49 +0545

    setting up tailwind css

---
+++ my-blog/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container bg-blue-500 text-center p-5">
+      App component
+     
+    </div>
+  );
+}
+
+export default App;
