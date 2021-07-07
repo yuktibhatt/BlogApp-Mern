e5be0479ff9bd5fc403cf255398cf141582086f5
yuktibhatt <bhattyukti5@gmail.com>
2021-07-07 22:41:36 +0545

    article list component

--- my-blog/src/pages/Article.js
+++ my-blog/src/pages/Article.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import articleContent from './article-content';
 
 const Article = ({ match }) => {
     const name = match.params.name;
+    const article = articleContent.find((article) => article.name === name);
+    if(!article) return <h1>Article does not exist.</h1>
     return (
         <>
          <h1 className=" sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-             This is a { name } article.
+            {article.title}
          
          </h1>
+         {article.content.map((paragraph, index) =>(
+             <p className="mx-auto leading-relaxed text-base mb-4">{paragraph}</p>
+        ))}
          </>
 
         
