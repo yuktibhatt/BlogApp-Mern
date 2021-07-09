aef74b01305736d13cd6424c5b9d687334de2b1d
yuktibhatt <bhattyukti5@gmail.com>
2021-07-09 23:09:32 +0545

    comment section

--- my-blog/src/pages/Article.js
+++ my-blog/src/pages/Article.js
@@ -2,11 +2,27 @@ import React from 'react';
 import articleContent from './article-content';
 import Articles from '../components/Articles';
 import NotFound from './NotFound'
-
+import { useState, useEffect } from 'react';
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 
 const Article = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find((article) => article.name === name);
+    const [ articleInfo, setArticleInfo ] = useState({ comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await fetch(`/api/articles/${name}`);
+          const body = await result.json();
+          console.log(body);
+          setArticleInfo(body);
+        };
+        fetchData();
+      }, [name]);
+
+
+
     if(!article) return <NotFound />;
     const otherArticles = articleContent.filter(
         (article) => article.name !== name
@@ -21,6 +37,8 @@ const Article = ({ match }) => {
              <p className="mx-auto leading-relaxed text-base mb-4">{paragraph}</p>
         ))}
 
+        <CommentsList comments= { articleInfo.comments} />
+        <AddCommentForm articleName={name} setAritcleInfo={setArticleInfo} />
         <h1 className="sm:text-2x text-xl font-bold mt-4 mb-4 text-gray-900">Other Articles</h1>
 
         <div className="flex flex-wrap -m-4">
